aa5dac96456e358a8f7aa3a8218e63820e6ce1f9
paulneider <paul.neider@mail.de>
2023-05-10 22:00:46 +0200

    Implement Convert Algorithm - implement algorithm for test purpose in the client - format MainWindow.xaml - add button style to disable the convert button on input errors - switch all resources to english - add check for the decimal places in the InputValidationRule.cs

--- DollarConverterClient/ValidationRules/InputValidationRule.cs
+++ DollarConverterClient/ValidationRules/InputValidationRule.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows.Controls;
 
 namespace DollarConverterClient;
@@ -10,23 +11,29 @@ internal class InputValidationRule : ValidationRule
         string? stringValue = value as string;
         if (stringValue is null)
         {
-            return new ValidationResult(false, "Keine Zeichenkette.");
+            return new ValidationResult(false, "Not a string.");
         }
 
         if (double.TryParse(stringValue, new CultureInfo("de-de"), out double input))
         {
             if (input < 0)
             {
-                return new ValidationResult(false, "Zahl muss größer als 0 sein.");
+                return new ValidationResult(false, "The number must be greater than or equal to 0.");
             }
             else if (input > 999_999_999.99)
             {
-                return new ValidationResult(false, "Zahl muss kleiner als 999.999.999,99 sein.");
+                return new ValidationResult(false, "The number must be less than or equal to 999.999.999,99.");
+            }
+
+            double decimals = input * 100;
+            if (Math.Ceiling(decimals) > decimals)
+            {
+                return new ValidationResult(false, "The number may have a maximum of two decimal places.");
             }
         }
         else
         {
-            return new ValidationResult(false, "Keine Zahl.");
+            return new ValidationResult(false, "No number.");
         }
 
 
